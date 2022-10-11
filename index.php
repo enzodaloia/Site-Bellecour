5a3546a2da1dc6c998971aae681aaea9e350dfe6
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-11 16:13:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,28 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Accueil</title>
 </head>
 <body>
     <h1>Contactez-nous</h1>
     <p>Pour toute demande, remplissez le formulaire ci-dessous.</p>
     <form method="POST">
+        <label>Nom</label>
+        <input type="text" name="nom" required>
         <label>Email</label>
         <input type="email" name="email" required>
         <label>Sujet</label>
         <input type="text" name="sujet" required>
         <textarea name="message" required></textarea>
-        <input type="text" value="Envoyer le message">
+        <input type="submit" value="Envoyer le message">
     </form>
-
-
-
-    <!-- <?php
-    $retour= mail("enzo.daloiadignazio@gmail.com", "essai", "Coucou les amies", "From:projetgb@projetg4bellecour.cf");
-    if($retour){
-        echo "<p> L'email à été envoyé !</p>";
+    <?php
+    if (isset($_POST["message"])) {
+        $message= "Ce message vous à été envoyé via la page du site projetg4bellecour.cf
+        Nom : " . $_POST["nom"] . "
+        Email : " . $_POST["email"] . "
+        Message : " . $_POST["message"];
+        $retour= mail("enzo.daloiadignazio@gmail.com", $_POST["sujet"], $message, "From:projetgb@projetg4bellecour.cf" . "\r\n" . "Reply-to:" . $_POST["email"]);
+        if($retour){
+            echo "<p> L'email à été envoyé !</p>";
+        }
     }
-
-    ?> -->
+    ?>
 </body>
 </html>
