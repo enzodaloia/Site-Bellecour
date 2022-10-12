3675a3ea2c83efb5c837ea397e992f3cd8d2a3fa
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 16:05:43 +0200

    changes

--- indexfr.php
+++ indexfr.php
@@ -1,37 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" type="text/css" >
     <title>Accueil</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Contactez-nous</h1>
-    <p>Pour toute demande, remplissez le formulaire ci-dessous.</p>
-    <form method="POST">
-        <label>Nom</label>
-        <input type="text" name="nom" required>
-        <label>Email</label>
-        <input type="email" name="email" required>
-        <label>Sujet</label>
-        <input type="text" name="sujet" required>
-        <label>Message</label>
-        <textarea name="message" required></textarea>
-        <input type="submit" value="Envoyer le message">
-    </form>
-    <?php
-    if (isset($_POST["message"])) {
-        $message= "Ce message vous à été envoyé via la page du site projetg4bellecour.cf
-        Nom : " . $_POST["nom"] . "
-        Email : " . $_POST["email"] . "
-        Message : " . $_POST["message"];
-        $retour= mail("enzo.daloiadignazio@gmail.com", $_POST["sujet"], $message, "From:projetgb@projetg4bellecour.cf" . "\r\n" . "Reply-to:" . $_POST["email"]);
-        if($retour){
-            echo "<p> L'email à été envoyé !</p>";
-        }
-    }
-    ?>
-</body>
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar scroll</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Link
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled">Link</a>
+            </li>
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </div>
+    </div>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    </body>
 </html>
