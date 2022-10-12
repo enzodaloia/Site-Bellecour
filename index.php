2abb7e373da5c21094b1989b39f87e096383c371
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 16:17:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar scroll</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarScroll">
+        <div class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
             <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
