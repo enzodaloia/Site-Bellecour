bebff16a75938ce9f1c975146f1874fa8e0e8082
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 16:16:39 +0200

    Update indexfr.php

--- indexfr.php
+++ indexfr.php
@@ -11,10 +11,10 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarScroll">
+        <div class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#"></a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
