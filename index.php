b7d1eba46b4064238dd95343ee0ae09d60a21552
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 16:19:36 +0200

    changes

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <div class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="contactfr.php">Contact</a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
