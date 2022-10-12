f1e84fa966b6b10334bfbc61da420e1ddd221cfb
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 20:44:18 +0200

    changes

--- index.php
+++ index.php
@@ -42,7 +42,45 @@
 
 
 
-
+	<header>
+		<div id="bannière1">
+		<h3><p>Verger Pédagogique du Lycée René Cassin</p>
+		<div id=box1accueil>
+		<a href="http://lyc71-cassin.ac-dijon.fr/"><p><img src="images/LOGOCASSIN.png"></p></a>
+		</div>
+		</div>
+			<div>
+				<ul>
+					<li><a href="Accueil.html">Accueil</a></li>
+					<li><a>projets faits</a>
+						<ul>
+							<li><a href="bancs.html">Bancs</a></li>
+							<li><a href="bacs.html">Bacs perma</a></li>
+							<li><a href="ruches.html">Ruches</a></li>
+							<li><a href="haie.html">Haie melifière</a></li>
+							<li><a href="arbre.html">Arbre solaire</a></li>
+							<li><a href="spirale.html">Spirale</a></li>
+						</ul>
+					</li>
+					<li><a>projets futurs</a>
+						<ul>
+							<li><a href="récupeau.html">Récup d'eau</a></li>
+							<li><a href="vignes.html">Les vignes</a></li>
+							<li><a href="hotelinsectev2.html">Hôtel d'insecte v2</a></li>
+						</ul>
+					</li>
+					<li><a href="photos.html">photos</a>
+						<ul>
+							<li><a href="photoeleves.html">élèves</a></li>
+							<li><a href="realisations.html">Réalisations</a></li>
+							<li><a href="annexes.html">Annexes</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		<div id="bannière2">  
+		</div>
+	</header>
 
 
 
