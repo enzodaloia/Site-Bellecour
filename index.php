ea7aea4dbfbef65e0b8ebb9347fb0f21588330af
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 20:25:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css" type="text/css" >
-    <title>Accueil</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse">
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="accueilfr.php">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="Bellecourfr.php">Bellecour</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="contactfr.php">Contact</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Histoire</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Link
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-            </li>
+
         </div>
         </ul>
     </div>
