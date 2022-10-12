077063e3da684ddde67eff15b95f5d4d2dd38c2c
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 21:01:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,36 +11,22 @@
 <header>
 		<div id="bannière1">
 		<div id=box1accueil>
-		<p><img src="Images/LogoBellecourOfficiel.png"></p>
+		<!-- <p><img src="Images/LogoBellecourOfficiel.png"></p> -->
 		</div>
 		</div>
 			<div>
 				<ul>
 					<li><a href="accueilfr.php">Accueil</a></li>
-					<li><a>projets faits</a>
+					<li><a>Activités</a>
 						<ul>
-							<li><a href="bancs.html">Bancs</a></li>
-							<li><a href="bacs.html">Bacs perma</a></li>
-							<li><a href="ruches.html">Ruches</a></li>
-							<li><a href="haie.html">Haie melifière</a></li>
-							<li><a href="arbre.html">Arbre solaire</a></li>
-							<li><a href="spirale.html">Spirale</a></li>
+							<li><a href="activitesfr.php">Activités</a></li>
+							<li><a href="transportfr.php">Transport</a></li>
 						</ul>
 					</li>
-					<li><a>projets futurs</a>
-						<ul>
-							<li><a href="récupeau.html">Récup d'eau</a></li>
-							<li><a href="vignes.html">Les vignes</a></li>
-							<li><a href="hotelinsectev2.html">Hôtel d'insecte v2</a></li>
-						</ul>
-					</li>
-					<li><a href="photos.html">photos</a>
-						<ul>
-							<li><a href="photoeleves.html">élèves</a></li>
-							<li><a href="realisations.html">Réalisations</a></li>
-							<li><a href="annexes.html">Annexes</a></li>
-						</ul>
-					</li>
+					<li><a href="histoirefr.php">Histoire</a></li>
+					<li><a href="actualitesfr.php">Actualités</a></li>
+                    <li><a href="contactfr.php">Contact</a></li>
+                    <li><a href="accesfr.php">Accès</a></li>
 				</ul>
 			</div>
 		<div id="bannière2">  
