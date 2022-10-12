468c53fc51c6b64bbd23a699d12caeb9dbbe959f
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-12 20:32:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,10 +19,8 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Bellecour</a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><a class="dropdown-item" href="#">Activités</a></li>
+                    <li><a class="dropdown-item" href="#">Transport</a></li>
                 </ul>
             </li>
             <li class="nav-item">
