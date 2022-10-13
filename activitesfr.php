78c63d638dfa7c2461024f5c7216625f1c1bdcff
Maxime Besson <maxime.besson15@gmail.com>
2022-10-13 16:13:44 +0200

    Update activitesfr.php

--- activitesfr.php
+++ activitesfr.php
@@ -34,16 +34,30 @@
 	</header>
     <body id="body">
 	
+	<h4>Activités</h4>
+<div class="container">
+
+	<div class="row row-cols-1 row-cols-md-3 g-5">
+	<div class="col">
+          <div class="card">
+            <img src="img/soigneur.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
         </div>
-
-
-
-
-
-
-
-
-
+        <div class="col">
+            <div class="card">
+              <img src="img/tank.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              </div>
+            </div>
+          </div>
+      </div>
+</div>
 
     <script src="js/bootstrap.bundle.min.js"></script>
     </body>
