1beefe36a2cdf6223b75313eaa429516db29d593
GolDoraK74 <113207855+GolDoraK74@users.noreply.github.com>
2022-10-17 08:51:24 +0200

    Update accueilfr.php

--- accueilfr.php
+++ accueilfr.php
@@ -1,78 +1,40 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="style.css" type="text/css" >
-<title>Accueil</title>
-<link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" >
+    <title>Accueil</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <header>
-<div>
-   <div class="Banniere1">
-     <img id="bannière1" src="img/img123.jpg">
-    </div>
-</div>
-<div id=box1accueil>
-<!-- <p><img src="Images/LogoBellecourOfficiel.png"></p> -->
-</div>
-
-
-    <div>
-        <ul>
-            <li><a href="accueilfr.php">Accueil</a></li>
-            <li><a>Activités</a>
-                <ul>
-                    <li><a href="activitesfr.php">Activités</a></li>
-                    <li><a href="transportfr.php">Transport</a></li>
-                </ul>
-            </li>
-            <li><a href="histoirefr.php">Histoire</a></li>
-            <li><a href="actualitesfr.php">Actualités</a></li>
-            <li><a href="contactfr.php">Contact</a></li>
-        <li><a href="accesfr.php">Accès</a></li>
-    </ul>
-</div>
-<div id="bannière2">    
-</div>
-</header>
-
-
-<body id="body">
-
-<!--* texte + titre-->
-<div id="titre" class="container-fluid">
-    <div class="position-absolute top-50 start-5 translate-middle">
-<h1> La Place Bellecour </h1>
-<p> La place Bellecour est la plus grande place de la ville de Lyon (62 000 m2), la cinquième plus grande place de France1 et la plus grande place piétonnière d'Europe. Elle présente une forme trapézoïdale avec une taille de 300 mètres sur 220 d'un côté et 190 mètres2 de l'autre.
-
-    En son centre se trouve une statue équestre de Louis XIV.
-    <br> Une autre statue, représentant Le Petit Prince et Antoine de Saint-Exupéry, est située à l'extrémité sud-ouest de la place. On y trouve aussi deux pavillons y abritent des services de l'office du tourisme, un petit parc pour enfants, une fontaine et deux brasseries. </br>
-</p>
-</div>
-</div>
-
-<!--* Image photo Bellecour -->
-<div id="statue" class="container-fluid">
-<div class="position-absolute top-50 start-100 translate-middle">
-<img src="img/img.jpg" class="img-fluid">
-
-</div>
-</div>
-
-<div id="titre">
-    <p>Cette place se trouve aujourd'hui dans le deuxième arrondissement de la ville sur la presqu'île, entre la Saône et le Rhône. De la place Bellecour partent trois artères majeures de la Presqu'île de Lyon, côté nord pour le quartier des Terreaux et côté sud pour le quartier de Perrache, dont deux piétonnières :
-    </p>
-    <ul><li>la Rue de la République menant à la Place de la Comédie (Lyon), entre l'Hôtel de Ville et l'Opéra puis à la place Louis-Pradel par la rue du Puits-Gaillot côté nord
-        <li>la rue Victor-Hugo, qui mène à Perrachecôté sud ;
-        <li>la rue du Président Édouard Herriot côté nord, ouverte à la circulation automobile, où se concentrent les enseignes de luxe, et qui mène à la place des Terreaux</a>.
-        </li>
-    </li>
-    </li>
-    </ul>
-
-</div>
+		<div id="bannière1">
+			<img src="img/statue.jpg">
+		<div id=box1accueil>
+		<!-- <p><img src="Images/LogoBellecourOfficiel.png"></p> -->
+		</div>
+		</div>
+			<div>
+				<ul>
+					<li><a href="accueilfr.php">Accueil</a></li>
+					<li><a>Activités</a>
+						<ul>
+							<li><a href="activitesfr.php">Activités</a></li>
+							<li><a href="transportfr.php">Transport</a></li>
+						</ul>
+					</li>
+					<li><a href="histoirefr.php">Histoire</a></li>
+					<li><a href="actualitesfr.php">Actualités</a></li>
+                    <li><a href="contactfr.php">Contact</a></li>
+                    <li><a href="accesfr.php">Accès</a></li>
+				</ul>
+			</div>
+		<div id="bannière2">  
+	    </div>
+	</header>
+    <body id="body">
+	
 
 
 
@@ -80,6 +42,9 @@
 
 
 
-<script src="js/bootstrap.bundle.min.js"></script>
-</body>
+
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    </body>
 </html>
