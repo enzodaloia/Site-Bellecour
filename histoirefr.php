83e3ed556b8338ca2e1d7d32baec46f2240451c3
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-17 11:45:50 +0200

    changes

--- histoirefr.php
+++ histoirefr.php
@@ -40,6 +40,11 @@
 
 
 <body id="body">
+<div id="test">
+		<p>/</p>
+	</div>
+	<div id="france"><a href="accueilfr.php"><p><img id="imgfr" src="Images/france.jpg"></p></a></div>
+	<div id="angleterre"><a href="accueilen.php"><p><img id="imgen" src="Images/angleterre.jpg"></p></a></div>
 
 <!--* texte + titre-->
 <div id="titre" class="container-fluid">
