b45ffc1f3457f41e5a63e924af3f742a75fe6f85
GolDoraK74 <113207855+GolDoraK74@users.noreply.github.com>
2022-10-20 15:14:44 +0200

    Update histoirefr.php

--- histoirefr.php
+++ histoirefr.php
@@ -59,12 +59,16 @@
 
 
 <article>
-    <p> La place Bellecour est la plus grande place de la ville de Lyon (62 000 m2), la cinquième plus grande place de France1 et la plus grande place piétonnière d'Europe.<br>
-         <br>Elle présente une forme trapézoïdale avec une taille de 300 mètres sur 220 d'un côté et 190 mètres2 de l'autre. <br>
-        En son centre se trouve une statue équestre de Louis XIV. <br>
-        Une autre statue, représentant Le Petit Prince et Antoine de Saint-Exupéry, est située à l'extrémité sud-ouest de la place. <br>
-        <br> On y trouve aussi deux pavillons y abritent des services de l'office du tourisme, un petit parc pour enfants, une fontaine et deux brasseries.
-</article>
+    <center>
+    <p>La place Bellecour est la plus grande place de Lyon (62 000 m²), la cinquième plus grande <br>
+         <br>place de France et la plus grande place piétonne d'Europe. Il est de forme trapézoïdale, <br>
+         mesurant 300 mètres sur 220 mètres d'un côté et 190 mètres de l'autre <br>
+         En son centre se trouve la statue équestre de Louis XIV. Une autre statue représentant le  <br>
+        <br>Petit Prince et Antoine de Saint-Exupéry est située à l'extrémité sud-ouest de la place. <br>
+        On y trouve également deux pavillons desservant l'office de tourisme, un petit parc pour enfants, une fontaine et deux bars.
+
+</center>
+    </article>
 </p>
 
 
@@ -73,114 +77,77 @@
 
 
 
-
+<div id="Localisation" class="container-fluid">
     <article>
-    <p id="texte">Cette place se trouve aujourd'hui dans le deuxième arrondissement de la ville sur la presqu'île, entre la Saône et le Rhône.<br>
-     De la place Bellecour partent trois artères majeures de la Presqu'île de Lyon, côté nord pour le quartier des Terreaux et côté sud pour le quartier de Perrache, dont deux piétonnières :</p>
+    <center><strong><h1> Localisation et accès </h1></strong></center>
+    <p id="texte">Cette place est aujourd'hui située dans le deuxième arrondissement de la ville sur la presqu'île, entre Saône et Rhône.<br>
+     De la place Bellecour partent les trois axes routiers principaux de la Presqu'île de Lyon, le quartier des Terreaux au nord et le quartier Perrache au sud dont deux rues piétonnes :<br>
+
+</p>
     <ul>
-        <li>la Rue de la République menant à la Place de la Comédie (Lyon), entre l'Hôtel de Ville et l'Opéra puis à la place Louis-Pradel par la rue du Puits-Gaillot côté nord
-        <li>la rue Victor-Hugo, qui mène à Perrachecôté sud ;
-        <li>la rue du Président Édouard Herriot côté nord, ouverte à la circulation automobile, où se concentrent les enseignes de luxe, et qui mène à la place des Terreaux</a>.
+        <li>la rue de la République, qui débouche sur la place de la Comédie entre la Mairie et l'Opéra
+        <li>puis sur la place Louis-Pradel par la rue du Puits-Gaillot côté nord
+        <li>la rue Victor-Hugo, menant à Perrache côté sud</a>.
         </li>
     </li>
     </li>
+    <p>
+    Côté nord, la rue du Président Édouard Herriot, où passent les voitures et où se concentrent les marques de luxe, débouche sur la place des Terreaux.<br>
+    <br>
+    La place est également située sur l'axe est-ouest reliant la rive droite du Rhône et la rive droite de la Saône. <br>
+    Le quartier du Vieux Lyon et la cathédrale Saint-Jean sont à l'ouest, de l'autre côté du pont Bonaparte sur la Saône, accessible par la rue du Colonel-Chambonnet.<br>
+     Le quartier de la Guillotière est à l'est, de l'autre côté du pont de la Guillotière, accessible par la rue de la Barre.<br>
+    <br> Deux places plus petites sont reliées à la Place Bellecour :<br>
+    <br>
+   <center><strong><h1>Odonymie:</h1></strong></center>
+    <p>Les noms de lieux avec le mot "Bellecour" sont apparus pour la première fois au XIIe siècle. Les terres agricoles appartenant à l'archevêque s'appellent Bella Curtis ("Le beau jardin"). Au XVIe siècle,<br>
+     ce qui n'était encore qu'un ranch s'appelait Pré de Belle Court. En 1715, elle devient place publique et prend le nom de Place Royale, puis Place Louis Legrand, en hommage à Louis XIV.<br>
+    <br>
+    En 1790, lors de la Révolution, le nom de la monarchie disparaît et la place devient la place de la Fédération pour perpétuer le souvenir de la Fête de la Fédération,<br>
+    dont l'anniversaire devient par la suite la Fête Nationale. A partir de 1793, la place des manifestations politiques de la ville est rebaptisée place de l'Égalité <br>
+    (en 1794, la place des Jacobins s'appelle place de la Fraternité). L'odonamem continue d'évoluer avec le système politique : en 1800, l'esplanade est la place Bonaparte,<br>
+    et logiquement plus tard elle devient la place Napoléon.<br>
+    <br>
+    <center><strong><h1>Histoire</h1></strong></center>
+    <p>
+    A l'époque gallo-romaine, Bellecour était une île ou presqu'île formée par des terres alluviales. <br>
+    Le quartier connaît à l'époque une activité romaine, militaire et commerciale : on y retrouve Cannabe,<br>
+    caserne 6 servant d'entrepôts aux marchands et marins gallo-romains, la ville est alors située sur la colline de Fourvière et descend sur la Saône au troisième.<br>
+    A la fin du XIIe siècle, l'archevêque de Lyon possédait Bella Curtis sur la presqu'île, constituée de terres à vignes. Abandonné, le lieu redevient marécageux.<br>
+    En 1562, le baron des Adrets attaque Lyon, installant ses hommes armés sur ce qui était alors "le pré de Belle-court".<br>
+    En 1604, Henri IV pousse le conseil municipal à acquérir la prairie pour y aménager une place publique.<br>
+    Mais dans un procès sans fin, les héritiers de l'archevêque se sont affrontés.<br>
+    <br>
+    En 1708, Louis XIV prend définitivement possession du site. La place Louis Legrand est ornée de statues en bronze représentant le roi, réalisées par Martin Desjardins.<br>
+    La place est entourée de bâtiments dont les façades ont été conçues par le premier architecte du roi Robert de Corte. En 1658, Louis XIV ne visite qu'une seule fois Lyon dans sa jeunesse.<br>
+    Depuis son installation à Versailles, le roi est présent en province, notamment à travers ses statues équestres.<br>
+    A la Révolution, un autel de la liberté y est érigé le 14 juillet 1790. Une guillotine y fut installée en 1792. La statue royale a été détruite en 1793.<br>
+    Le 21 juin 1800, Bonaparte passe par Lyon après sa victoire à Marengo et pose la première pierre du nouvel édifice.<br>
+    Lors de la restauration, la place est ornée d'une nouvelle statue de Louis XIV, inaugurée le 6 novembre 1825, par le sculpteur François-Frédéric Lemot.<br>
+    En 1829, Lady Girard établit la célèbre buvette sur la place.<br>
+    <br>
+    Les deux pavillons de la place ont été construits par Tony Desjardins en 1852.<br>
+     A l'ouest, près du jardin d'enfants, avant de devenir les bureaux de l'office de tourisme "Le Rectangle",<br>
+      se trouve une salle d'exposition appelée Maison de Lyon.<br>
+       Il a commencé comme un café appelé La gargote du Père Lathuille et est devenu plus tard un café-restaurant de danse appelé La Maison Dorée.<br>
+        Le bâtiment au sud-est de la place, dans lequel s'est installé l'Office de Tourisme en 2008, était un kiosque militaire, transformé plus tard<br>
+         en commissariat puis en 19247 en Syndicat d'Initiative.<br>
+<br>
+En 1934, l'Hôpital de la Charité est détruit, à l'exception du clocher de l'église. <br>
+La place de la Charité a ensuite été agrandie pour devenir l'actuelle place Antonin-Ponce.<br>
+<br>
+
+
+
+
+</p>
     </ul>
 </article>
 </div>
 
+<img src="Images/statu.jpg"
 
 
-
-<div id="Localisation" class="container-fluid">
- <h1> Localisation et accès </h1>
-
- <div id="article" class="container-fluid">
-     <article>
-    <p>Cette place se trouve aujourd'hui dans le deuxième arrondissement de la ville sur la presqu'île, entre la Saône et le Rhône. <br>
-    De la place Bellecour partent trois artères majeures de la Presqu'île de Lyon, côté nord pour le quartier des Terreaux et côté sud pour le quartier de Perrache, dont deux piétonnières :</p>
-    <li>la rue de la République, menant à la place de la Comédie, entre l'Hôtel de Ville et l'Opéra puis à la place Louis-Pradel par la rue du Puits-Gaillot côté nord</li>
-    <li>la rue Victor-Hugo, qui mène à Perrache côté sud</li>
-    <li>la rue du Président Édouard Herriot côté nord, ouverte à la circulation automobile, où se concentrent les enseignes de luxe, et qui mène à la place des Terreaux.</li>
-
-    <p>La place est également située sur l'axe est-ouest permettant de relier la rive droite du Rhône à la rive droite de la Saône. <br>
-    Le quartier du Vieux Lyon et de la cathédrale Saint-Jean se situent à l'ouest, de l'autre côté du pont Bonaparte sur la Saône, que l'on rejoint par la rue du Colonel-Chambonnet. <br>
-    Le quartier de La Guillotière se situent à l'est, de l'autre côté du pont de la Guillotière que l'on rejoint par la rue de la Barre.
-</p>
-</div>
-
-<!--* odonymie -->
-<br>
-<br>
-<h1>Odonymie</h1>
-<article>
-<p> Le toponyme dont dérive le mot « Bellecour » apparaît dès le xiie siècle. Les terres agricoles que l'archevêque y possède sont appelées bella curtis (« beau jardin »). <br>
-Au xvie siècle, ce qui n'est encore qu'un pâturage porte le nom de pré de Belle court4. En 1715, devenue place publique, <br>
-elle est baptisée place Royale, puis place Louis-le-Grand en hommage à Louis XIV.</p>
-<br> En 1790, sous la Révolution, les odonymes monarchistes disparaissent et la place devient place de la Fédération, pour perpétuer le souvenir de la Fête de la Fédération, <br>
-dont l'anniversaire marque depuis lors la fête nationale française.<br>
-Dès 1793, le lieu des manifestations politiques de la ville prend le nouveau nom de place de l’Égalité (en 1794, la place des Jacobins s'appelle place de la Fraternité). <br>
-L'odonyme continue à évoluer avec les régimes politiques : 
-en 1800 l'esplanade est la place Bonaparte et devient logiquement un peu plus tard place Napoléon <br>
-(avant que, sous le Second Empire, ce ne soit la place Carnot à laquelle ce nom est attribué).</br>
-<br> En 1814, sous la Restauration, la place redevient place Louis-le-Grand. Ce n'est que sous la Troisième République que la place prend son nom actuel de place Bellecour. <br>
-Elle est renommée place du Maréchal-Pétain après une visite du chef du régime de Vichy à Lyon les 18 et 19 novembre 19405. Elle reprend le nom de place Bellecour à la Libération.</br>
-</article>
-</article>
-
-
-<br>
-<br> 
-
-<h1> Histoire </h1>
-<article>
-    <p>
-    À l’époque gallo-romaine, le quartier de Bellecour est une île ou une presqu'île formée de terres alluviales.<br>
-    Le quartier connait alors une activité romaine, militaire et marchande : <br>
-    on y a retrouvé des canabae, baraques qui servaient d’entrepôt aux négociants et aux nautes gallo-romains6,<br>
-<br>
-    la ville se situant alors sur la colline de Fourvière, avant qu'elle en descende en rive de la Saône au IIIe.<br>
-    À la fin du XIIe, l'archevêque de Lyon possède sur la presqu'île la bella curtis, composée de terrains où pousse notamment une vigne. <br>
-    Abandonné, l'endroit redevient marécageux.<br>
-    En 1562, le baron des Adrets attaquant Lyon, installe ses hommes d'arme sur ce qui est alors « le pré de Belle-court »4.<br>
-    En 1604, Henri IV pousse le Conseil de la ville à acquérir le pré afin d'y aménager une place publique. <br>
-    Mais les héritiers de l'archevêque se disputent lors d'un interminable procès.<br>
-<br>
-    L'Aumône générale achète le 2 décembre 1614 des terrains situés entre la place Bellecour et le Rhône afin d'y construire un hôpital.<br>
-    L'hôpital de la Charité de Lyon est construit de 1617 à 1622. L'église de l'hôpital s'ouvre sur la place Bellecour. <br>
-    La place de la Charité est aménagée entre l'angle sud-est de la place Bellecour et les quais du Rhône, le long du flanc nord de l'hôpital.<br>
-    En 1708, Louis XIV obtient la possession définitive de la place. <br>
-    La place Louis-le-Grand est alors ornée d'une statue de bronze représentant le roi et réalisée par Martin Desjardins.<br>
-    Autour de la place sont construits des édifices, dont les façades sont dessinées par le premier architecte du roi Robert de Cotte.<br>
-    Louis XIV visite Lyon une seule fois, dans sa jeunesse, en 1658. Depuis l'installation à Versailles,<br>
-    le roi est présent en province surtout par ses statues équestres.<br>
-    À la Révolution, un autel de la Liberté y est élevé le 14 juillet 1790. On y installe une guillotine en 1792. La statue royale est détruite en 1793.<br>
-    Le 21 juin 1800, Bonaparte de passage à Lyon après la victoire de Marengo, pose la première pierre des nouveaux édifices.<br>
-    Lors de la Restauration, la place s'orne d'une nouvelle statue de Louis XIV, due au sculpteur François-Frédéric Lemot, inaugurée le 6 novembre 1825.<br>
-    En 1829, Madame Girard installe une buvette de renommée sur la place.<br>
-    La place est indirectement concernée par les travaux de transformation de la presqu'île menés par le préfet Vaïsse pendant le Second empire.<br>
-    Quelques immeubles sont démolis au nord de la place pour permettre l'ouverture de nouvelles voies à partir de la place Bellecour : <br>
-<br>
-      <li> élargissement de la rue Bellecordière, incorporée à la nouvelle rue Impériale (rue de la République) vers la place de la Comédie  </li>
-      <li> création de la rue de l'Impératrice (rue Édouard-Herriot) vers la place des Terreaux </li>
-      <li> élargissement de la place Le Viste à la jonction des deux précédentes rues </li>
-      <li>création de la rue Gasparin à l'emplacement d'une partie de l'ancienne préfecture (ancien couvent des Jacobins</li>
-<br>
- Les deux pavillons situés sur la place ont été construits en 1852 par Tony Desjardins. <br>
- Celui qui est à l'ouest, à côté des jardins d'enfants, était, avant de devenir les bureaux de l'office de tourisme « Le Rectangle », <br>
- une salle d'exposition nommée Maison de Lyon. C'était initialement un café, La gargote du Père Lathuille, avant de devenir un café-restaurant dansant nommé La Maison Dorée.<br>
- Le bâtiment situé au sud-est de la place, dans lequel a été installé l'office du tourisme en 2008,<br>
-  était un pavillon militaire, plus tard transformé en poste de police, puis en Syndicat d'Initiative en 1924.<br>
- En 1934, l'hôpital de la Charité est détruit, à l'exception du clocher de l'église. La place de la Charité est alors agrandie et devient l'actuelle place Antonin-Poncet.<br>
-<br>
-Depuis 2011, d'importants travaux d'embellissement ont lieu sur la partie sud de la place. Au programme : 
-      <li> déplacements des deux bassins d'eau </li>
-      <li> rénovation des cinq kiosques sur le côté est et construction d'un nouveau kiosque sur le côté ouest</li>
-      <li> rénovation de l'aire de jeux pour enfants, nouvel éclairage (lampadaires à LEDs)</li>
-      <li> plantation de haies et d'arbres (remplacement des marronniers par des tilleuls et des merisiers)</li>
-<br>
-Il est à noter que le tilleul est la première essence plantée sur la place au xviie siècle.
-</p>
-</article>
 
 
 <script src="js/bootstrap.bundle.min.js"></script>
