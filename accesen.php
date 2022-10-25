6ff300bacafd56c782ab84f35768fc0eeaa54c15
Maxime Besson <maxime.besson15@gmail.com>
2022-10-25 16:17:43 +0200

    Create accesen.php

---
+++ accesen.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" >
+    <title>Access</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="screen.css" media="screen">
+<link rel="stylesheet" href="impression.css" type="text/css" media="print">
+</head>
+<header>
+<div id="bannière1">
+    <img id="bannière1" src="Images/banniere1.jpg" class="img-top">
+   </div>
+
+			<div id="ecrit">
+				<ul>
+					<li><a href="accueilen.php">Home</a></li>
+					<li><a href="activitesen.php">Activities</a>
+					</li>
+					<li><a href="histoireen.php">Story</a></li>
+					<li><a href="actualitesen.php">News</a></li>
+                    <li><a href="contacten.php">Contact</a></li>
+                    <li><a href="accesen.php">Access</a>
+					<ul>
+							<li><a href="transporten.php">Transport</a></li>
+					</ul>
+					</li>
+				</ul>
+			</div>
+			
+		<div id="bannière2">
+	    </div>
+	</header>
+    <body id="body">
+
+	<div id="test">
+		<p>/</p>
+	</div>
+	<div id="france"><a href="accesfr.php"><p><img id="imgfr" src="Images/france.jpg"></p></a></div>
+	<div id="angleterre"><a href="accesen.php"><p><img id="imgen" src="Images/angleterre.jpg"></p></a></div>
+
+	<div class="FormActivité">
+<center><h1>Location of the Bellecour square</h1>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.666440234341!2d4.8300076155673155!3d45.
+75783267910552!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea537bdc5ab3%3A0x5d9d9385f58b2047!2sPlace
+%20Bellecour!5e0!3m2!1sfr!2sfr!4v1666248701164!5m2!1sfr!2sfr" width="1400" height="250" style="border:0;" allowfullscreen="" 
+loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></iframe></center>
+
+</div>
+</div>
+</div>
+<div style="float:right;"><img src="img/captcha petit.jpg" class="imageflottante" alt="..."></div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
