3b9a4b6e93be730fbf678dacbdb0900b5615463f
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2022-10-25 11:07:27 +0200

    Update accueilfr.php

--- accueilfr.php
+++ accueilfr.php
@@ -184,23 +184,11 @@
 
 						<footer class="bg-light text-center text-white">
   <!-- Grid container -->
-    <button class="btn btn-danger" type="button">
-      <i class="fa-brands fa-youtube"></i>
-    </button>
-    <button class="btn btn-info" type="button">
-      <i class="fa-brands fa-twitter text-white"></i>
-    </button>
     <button class="btn btn-primary" type="button">
       <i class="fa-brands fa-facebook"></i>
     </button>
     <button class="btn btn-danger" type="button">
       <i class="fa-brands fa-instagram"></i>
-    </button>
-    <button class="btn btn-success" type="button">
-      <i class="fa-brands fa-whatsapp"></i>
-    </button>
-    <button class="btn btn-dark" type="button">
-      <i class="fa-brands fa-github"></i>
     </button>
   <!-- Grid container -->
 
